589734c674a0adf91e4e14ac094b3753f8bdee1d
Nicholas Chang <37339122+nicholas0417@users.noreply.github.com>
2020-04-01 00:08:22 +0800

    wrong hierachy

--- data_types/product.py
+++ data_types/product.py
@@ -4,6 +4,6 @@ num2 = int (input("please enter number2:"))
 product = num1 * num2
 # If product is greater than 1000
 if (product < 1000):
-        print("The product is : " product)
+    print("The product is : " product)
 else:
-        print(num1 + num2)
+    print(num1 + num2)
